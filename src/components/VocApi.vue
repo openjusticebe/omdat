c5d55ba5dd25204173fb2d259372e3e759821509
Martin Erpicum <martin@erpicum.net>
2022-05-13 22:01:40 +0200

    cleaning code and adding loading status

--- src/components/VocApi.vue
+++ src/components/VocApi.vue
@@ -1,8 +1,17 @@
 <template>
-  <h1>VocApi</h1>
-  <li v-for="(field, index) in fields.tree" :key="index" class="col-md-5 small">
-    {{ field }}
-  </li>
+  <div v-if="!data_fetched">loading ...</div>
+  <div class="col-12" v-else>
+    <h3>📔 Voc Api</h3>
+    <hr />
+    <span
+      v-for="(field, index) in fields.tree"
+      :key="index"
+      class="col-md-5 small"
+    >
+      {{ field.labels.fr
+      }}<sup><a :href="field.iri" target="_blank">⚓︎</a></sup>
+    </span>
+  </div>
 </template>
 
 <script>
